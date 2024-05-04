1aa9273e7624c5b98cabde4477a76aca95e1a67d
lcaohoanq <hoangclw@gmail.com>
2024-05-04 10:59:37 +0700

    chore: implement fetch product status data from node-red

--- src/module/product/product.route.ts
+++ src/module/product/product.route.ts
@@ -1,4 +1,5 @@
-import { Router } from 'express';
+import axios from 'axios';
+import { Request, Response, Router } from 'express';
 import { getAllController, getByIdController } from './product.controller';
 import { getByIdValidator } from './product.middleware';
 
@@ -9,5 +10,18 @@ productRouter.get('/all', getAllController);
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 productRouter.get('/:id', getByIdValidator, getByIdController);
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+productRouter.get('/en/status', async (_: Request, res: Response) => {
+  try {
+    const response = await axios.get(
+      'https://dreameclonetraining-communication-server.onrender.com/api/product_en/status',
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching data from external API');
+  }
+});
 
 export default productRouter;
